887ed3a1cb35ea2a2a641880f8e2fcd87f6c1a26
shashankshenvi <shashankshenvi04@gmail.com>
2025-03-02 22:36:25 +0530

    pom and vite.config changes

--- FrontEnd/portfolio-frontend/vite.config.js
+++ FrontEnd/portfolio-frontend/vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Ensure the base URL is set correctly
   plugins: [react(), tailwindcss()],
   css: {
     modules: {
       localsConvention: "camelCase",
     },
   },
-  base: "/PORTFOLIO/",
 });
